5c3216c0e8f3a090e7aef093b1e3985dd05b2347
theKyrgyz <43068216+theKyrgyz@users.noreply.github.com>
2018-09-07 09:04:34 -0400

    Initial commit

---
+++ first.py
@@ -0,0 +1,13 @@
+print("who's a milky piggy? me me me")
+
+responsePiggy = input("Who's a milky piggy? ")
+
+print("Nice to meet you, "+responsePiggy+"!")
+
+responseClass = input("What is your favorite class? ")
+
+print("Uh huh, yeah, I don't really like "+responseClass+".")
+
+responseNumber = input("Listen. I gotta run. What's your number? ")
+
+print ("Alright. Got it. I'll text you later.")
