c47d5b0a829052b0a5c161ded1d691e6bf3fb3bc
theKyrgyz <43068216+theKyrgyz@users.noreply.github.com>
2018-10-26 09:12:54 -0400

    Banking v0.1
    Banking done in-class.

---
+++ AxolotlPetSim2018/axolotlClass.py
@@ -0,0 +1,93 @@
+# axolotlClass.py - class file for the axolotl pet
+
+# plan out your code
+"""
+PROPERTIES: (ex. dog):
+breed, size, color, name, home, food/diet, owner, skills/tricks, energy level, hunger/needs, captivity/no
+Needs: energy, hunger, thirst, happiness
+
+METHODS: (things it can do):
+walk, eat, sleep, play, fetch, roll over, bark, pee
+
+NOW: create the dog class - the general plan for the object
+"""
+import random
+
+class axolotl:
+    kind = 'axolotl pet'
+    # constructor:
+    # initializes properties, sets up the animal object
+    def __init__(self, number, name="John", wild=False, loc="Minneapolis", hunger=4, energy=4):
+        #, number, name, wild, location
+        self.number = number
+        self.name = name
+        self.wild = wild
+        self.loc = loc
+        self.hunger = hunger
+        self.energy = energy
+
+    # methods / functions
+
+    def eat(self, amt):
+        status = ""
+        if amt == 0:
+            print("You didn't pick anything that "+self.name+" could eat.")
+            mainLoop()
+        if self.hunger > 0:
+            self.hunger -= int(2*amt)
+            self.energy += int(1*amt)
+            if self.hunger < 0:
+                self.hunger = 0
+            if self.energy > 10:
+                self.energy = 10
+            status = self.name+" just that sweet sweet yum yum."
+        else:
+            status = self.name+" wouldn't take a single bite."
+
+    def checkWild(self):
+        if self.wild == True:
+            return "In the wild"
+        else:
+            return "In captivity"
+
+    def stats(self):
+        return "\nYOUR PET AXOLOTL:\n"+"Name: "+self.name+"\nCaptivity Status: "+self.checkWild()+"\nLocation: "+self.loc+"\nHunger: "+str(self.hunger)+"\nEnergy: "+str(self.energy)
+
+"""
+def showStats(pet):
+    print("\nYOUR PET AXOLOTL:\n"+"Name: "+pet.name+"\nCaptivity Status: "+checkWild(pet.wild)+"\nLocation: "+pet.loc+"\nHunger: "+pet.hunger+"\nEnergy: "+pet.energy)
+"""
+
+def howMuchEat():
+    foodInput = input("What would you like to feed your axolotl? \n(1) A food pellet. \n(2) A squirmy worm. \n(3) A whole freakin' fish.\n>> ")
+    if foodInput < 4:
+        return int(foodInput)
+    else:
+        return 0
+
+PetNames = ["Xochitl","Benny","Tizoc","Tlaco","Yaotl","Ruben","Jacobin"]
+CaptiveLocs = ["El Paso","Dallas","Washington","Bordeaux","Shreveport","Galveston","Phoenix"]
+WildLocs = ["Xochimilco","Chalco","Mexico City","Tlaxcala"]
+
+A1 = axolotl(1, "Xochitl", False, "El Paso", 5, 5)
+
+def creationProcess():
+    isWild = random.choice([True, False])
+    if isWild == True:
+        P1 = axolotl(2, random.choice(PetNames), True, random.choice(WildLocs), random.randint(2,6), random.randint(3,7))
+    else:
+        P1 = axolotl(2, random.choice(PetNames), False, random.choice(CaptiveLocs), random.randint(3,7), random.randint(2,6))
+
+creationProcess()
+
+print(P1.__dict__)
+
+def mainLoop(): 
+    while True:
+        print(P1.stats()+"\n")
+        choice = input("Choose how you'd like to care for your pet.\n>> ")
+        if choice == "eat":
+            P1.eat(howMuchEat())
+        else:
+            print("I don't quite understand that.")
+mainLoop()
---
+++ banking.py
@@ -0,0 +1,76 @@
+# banking.py
+
+import random
+
+def askName():
+    NameSelection = input("Please input the name you want associated with your account.\n>> ")
+    return NameSelection
+
+def askPin():
+    while True:
+        PinSelection = input("Please input the 4-digit pin you want associated with your account.\n>> ")
+        if len(PinSelection) == 4:
+            return int(PinSelection)
+
+def genID():
+        y = "0"
+        for i in range(0,5):
+            z = str(random.randint(0,9))
+            y += z
+        return str(y)
+
+class Account:
+    kind = 'bank account'
+    # constructor:
+    # initializes properties, sets up the animal object
+    def __init__(self):
+        #, number, name, wild, location
+        self.ID = genID()
+        self.holdername = askName()
+        self.PIN = askPin()
+        self.amount = 0
+
+    # methods / functions
+
+    def stats(self):
+        print("\n")
+        print("ACCOUNT NAME: "+self.holdername)
+        print("ACCOUNT BALANCE: $"+str(self.amount))
+        print("\n")
+
+    def deposit(self, amtToAdd):
+        self.amount += amtToAdd
+        print("Deposit was successful. Current balance is: $"+str(self.amount)+"\n")
+
+    def checkPin(self, guess):
+        if str(g) == str(self.PIN):
+            return True
+        else:
+            return False
+
+listAccounts = []
+
+listAccounts.append(Account())
+
+print(listAccounts[0].__dict__)
+
+def inputLoop():
+    while True:
+        listAccounts[vCurr].stats()
+        choice = input("(1) Make a deposit.\n(2) Make a withdrawl.\n(3) Change PIN.\n(4) Close account.\n>> ")
+        if choice == "1":
+            global g
+            g = input("Please input your PIN.")
+            if listAccounts[vCurr].checkPin(g) == True:
+                x = input("Please state how much of the revenue from your exploited labor you'd like to deposit.\n>> ")
+                if int(x) != ValueError:
+                    listAccounts[vCurr].deposit(int(x))
+                else:
+                    print("That's not a number.")
+            else:
+                print("\nIncorrect PIN. I'm calling the police.\n")
+        else:
+            print("I don't quite understand that.")
+
+vCurr = 0
+inputLoop()
